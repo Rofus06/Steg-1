7f3bd2f811c2a658cdc069e50eb8f3a91d1ed8fc
Rofus06 <Filipsmail@yahoo.com>
2024-11-14 11:24:49 +0100

    first commit

---
+++ Steg 1/Program.cs
@@ -0,0 +1,33 @@
+﻿Random random = new Random();
+int correctNumber = random.Next(1, 101);  // Random number between 1 and 100
+int userGuess = 0;
+bool isGuessedCorrectly = false;
+
+Console.WriteLine("Guess the number (between 1 and 100):");
+
+while (!isGuessedCorrectly)
+{
+    Console.Write("Enter your guess: ");
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out userGuess))
+    {
+        if (userGuess < correctNumber)
+        {
+            Console.WriteLine("Your guess is too low!");
+        }
+        else if (userGuess > correctNumber)
+        {
+            Console.WriteLine("Your guess is too high!");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations! You've guessed the right number!");
+            isGuessedCorrectly = true;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number.");
+    }
+}
